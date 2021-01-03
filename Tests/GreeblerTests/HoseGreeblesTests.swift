62029ea72f716c039bf10c71fb484d6f237bd223
B.T. Franklin <brandon.franklin@gmail.com>
2021-01-02 19:22:25 -0700

    Updated unit tests for hose and panel greebles.

--- Tests/GreeblerTests/HoseGreeblesTests.swift
+++ Tests/GreeblerTests/HoseGreeblesTests.swift
@@ -11,7 +11,24 @@ final class HoseGreeblesTests: XCTestCase {
         let grayThemeColor = CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
         let graphicsContext = GreeblesTestUtil.createGraphicsContext()
 
-        let hoseGreebles = HoseGreebles(xUnits: 3, themeColor: grayThemeColor.hsbaColor, hoseCount: 3, allowOffSide: false)
-        hoseGreebles.draw(on: graphicsContext)
+        for unitSize in stride(from: CGFloat(1), through: CGFloat(1000), by: 10) {
+            for hoseCount in stride(from: 1, through: 100, by: 10) {
+                let hoseGreebles = HoseGreebles(xUnits: unitSize, yUnits: unitSize, themeColor: grayThemeColor.hsbaColor, hoseCount: hoseCount, allowOffSide: false)
+                hoseGreebles.draw(on: graphicsContext)
+            }
+        }
     }
+
+    func testDrawAllowOffSide() {
+        let grayThemeColor = CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+        let graphicsContext = GreeblesTestUtil.createGraphicsContext()
+
+        for unitSize in stride(from: CGFloat(1), through: CGFloat(1000), by: 10) {
+            for hoseCount in stride(from: 1, through: 100, by: 10) {
+                let hoseGreebles = HoseGreebles(xUnits: unitSize, yUnits: unitSize, themeColor: grayThemeColor.hsbaColor, hoseCount: hoseCount, allowOffSide: true)
+                hoseGreebles.draw(on: graphicsContext)
+            }
+        }
+    }
+
 }
---
+++ Tests/GreeblerTests/PanelGreeblesTests.swift
@@ -0,0 +1,24 @@
+//  Created by B.T. Franklin on 1/2/21.
+
+import XCTest
+import CoreGraphics
+import Aesthete
+@testable import Greebler
+
+final class PanelGreeblesTests: XCTestCase {
+
+    func testDraw() {
+        let grayThemeColor = CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+        let graphicsContext = GreeblesTestUtil.createGraphicsContext()
+
+        for unitSize in stride(from: CGFloat(1), through: CGFloat(1000), by: 10) {
+            for panelCount in stride(from: 2, through: 100, by: 10) {
+                let panelGreeblesNoRivets = PanelGreebles(xUnits: unitSize, yUnits: unitSize, themeColor: grayThemeColor.hsbaColor, panelCount: panelCount, showRivets: false)
+                panelGreeblesNoRivets.draw(on: graphicsContext)
+
+                let panelGreeblesWithRivets = PanelGreebles(xUnits: unitSize, yUnits: unitSize, themeColor: grayThemeColor.hsbaColor, panelCount: panelCount, showRivets: true)
+                panelGreeblesWithRivets.draw(on: graphicsContext)
+            }
+        }
+    }
+}
